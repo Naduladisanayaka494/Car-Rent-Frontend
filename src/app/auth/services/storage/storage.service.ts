334ab25e7eeabfd45f51e06f642bf89a490a2df6
Naduladisanayaka494 <it20247140@my.sliit.lk>
2024-07-05 12:20:01 +0530

    integraed login apis and save token in stroge and navigate to respective pages

--- src/app/auth/services/storage/storage.service.ts
+++ src/app/auth/services/storage/storage.service.ts
@@ -1,9 +1,59 @@
 import { Injectable } from '@angular/core';
 
+const TOKEN = "token";
+const USER = "user";
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StorageService {
+  constructor() {}
 
-  constructor() { }
+  static saveToken(token: string): void {
+    window.localStorage.removeItem(TOKEN);
+    window.localStorage.setItem(TOKEN, token);
+  }
+
+  static saveUser(user: any): void {
+    window.localStorage.removeItem(USER);
+    window.localStorage.setItem(USER, JSON.stringify(user));
+  }
+
+  static getToken() {
+    return window.localStorage.getItem(TOKEN);
+  }
+  static getUser() {
+    const userJson = localStorage.getItem(USER);
+    if (userJson) {
+      return JSON.parse(userJson);
+    }
+    return null;
+  }
+
+  static getUserRole() {
+    const user = this.getUser();
+    if (user === null) {
+      return '';
+    } else {
+      return user.role;
+    }
+  }
+
+  static isAdminLoggedIn(): boolean {
+    if (this.getToken() == null) {
+      return false;
+    } else {
+      const role = (String = this.getUserRole());
+      return role == 'ADMIN';
+    }
+  }
+
+  static isCustomerLoggedIn(): boolean {
+    if (this.getToken() == null) {
+      return false;
+    } else {
+      const role = (String = this.getUserRole());
+      return role == 'CUSTOMER';
+    }
+  }
 }
--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponenetComponent } from './components/admin-componenet/admin-componenet.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"dashboard",component:AdminComponenetComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
